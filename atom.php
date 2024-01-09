0cf0d1d6defac840d0dca08fa317ee59c60f7a04
FDHoho007 <mail@fdhoho007.de>
2024-01-09 14:07:14 +0100

    Cleanup and fix atom feed.

--- atom.php
+++ atom.php
@@ -2,25 +2,26 @@
 global $db;
 require "lib.php";
 header("Content-Type: application/atom+xml");
-echo("<?xml version=\"1.0\" encoding=\"utf-8\"?>");
-echo("<feed xmlns=\"http://www.w3.org/2005/Atom\">");
-echo("<title>Enten Tagebücher</title>");
-echo("<link href=\"https://ducks.fs-info.de/\" rel=\"alternate\" />");
-echo("<id>https://ducks.fs-info.de/</id>");
-echo("<updated>" . date("c", strtotime($db->query("SELECT timestamp FROM diary ORDER BY timestamp DESC LIMIT 1;")["timestamp"])) . "</updated>");
+echo("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n");
+echo("<feed xmlns=\"http://www.w3.org/2005/Atom\">\n");
+echo("  <title>Enten Tagebücher</title>\n");
+echo("  <link href=\"https://ducks.fs-info.de/\" rel=\"alternate\" />\n");
+echo("  <link href=\"https://ducks.fs-info.de/atom.php\" rel=\"self\" />\n");
+echo("  <id>https://ducks.fs-info.de/</id>\n");
+echo("  <updated>" . date("c", strtotime($db->query("SELECT timestamp FROM diary ORDER BY timestamp DESC LIMIT 1;")["timestamp"])) . "</updated>\n");
 foreach($db->queryAll("SELECT * FROM ducks ORDER BY name ASC;") as $duck) {
     $duckName = $duck["name"];
     foreach ($db->queryAll("SELECT id, timestamp, author, entry FROM diary WHERE duck=? ORDER BY timestamp DESC;", $duckName) as $entry) {
-        echo("<entry>");
-        echo("<id>https://ducks.fs-info.de/" . str_replace(" ", "_", strtolower($duckName)) . "#" . $entry["id"] . "</id>");
-        echo("<link href=\"https://ducks.fs-info.de/" . str_replace(" ", "_", strtolower($duckName)) . "#" . $entry["id"] . "\" rel=\"alternate\" \>");
-        echo("<title>Tagebucheintrag für $duckName von " . $entry["author"] . "</title>");
-        echo("<author><name>" . $entry["author"] . "</name></author>");
-        echo("<content>" . $entry["entry"] . "</content>");
-        echo("<updated>" . date("c", strtotime($entry["timestamp"])) . "</updated>");
-        echo("<category term=\"$duckName\" />");
-        echo("</entry>");
+        echo("  <entry>\n");
+        echo("      <id>https://ducks.fs-info.de/" . str_replace(" ", "_", strtolower($duckName)) . "#" . $entry["id"] . "</id>\n");
+        echo("      <link href=\"https://ducks.fs-info.de/" . str_replace(" ", "_", strtolower($duckName)) . "#" . $entry["id"] . "\" rel=\"alternate\" />\n");
+        echo("      <title>Tagebucheintrag für $duckName von " . $entry["author"] . "</title>\n");
+        echo("      <author><name>" . $entry["author"] . "</name></author>\n");
+        echo("      <content>" . $entry["entry"] . "</content>\n");
+        echo("      <updated>" . date("c", strtotime($entry["timestamp"])) . "</updated>\n");
+        echo("      <category term=\"$duckName\" />\n");
+        echo("  </entry>\n");
     }
 }
-echo("</feed>");
+echo("</feed>\n");
 ?>
